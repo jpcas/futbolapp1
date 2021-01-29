9ecc6f94a67fcb15bce59bf29d1823df42eda8f5
JP Castrillon <jpcas001@gmail.com>
2021-01-29 13:59:53 -0500

    added navbar component & added css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage';
+import NavBar from './components/NavBar';
 
 const App = () => (
 <BrowserRouter>
+<NavBar />
 <Switch>
 <Route exact path="/" component={HomePage} />
 </Switch>
