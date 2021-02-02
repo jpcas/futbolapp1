394fcad9331602eae68fff0abd365cd327ece632
JP Castrillon <jpcas001@gmail.com>
2021-02-01 19:05:19 -0500

    added dropdowns for the leagues and tables

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,26 +5,31 @@ const NavBar = () => {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">FutbolApp</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+    <Nav className="navbar">
+      <Nav.Link href="#features">Scores</Nav.Link>
+      <Nav.Link href="#pricing">Schedule</Nav.Link>
+      <Nav.Link href="#pricing">Transfers</Nav.Link>
+      <NavDropdown title="Leagues" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.3">Champions League</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">MLS</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.1">EPL</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">La Liga</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Serie A</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Bundesliga</NavDropdown.Item>
+      </NavDropdown>
+      <NavDropdown title="Tables" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.3">Champions League</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">MLS</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.1">EPL</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">La Liga</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Serie A</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Bundesliga</NavDropdown.Item>
       </NavDropdown>
     </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
+    
   </Navbar.Collapse>
 </Navbar>
         </div>
